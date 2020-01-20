09d13fd56a5455996ec95eb7b88d5cc9599300cc
xihajun <mw18386@my.bristol.ac.uk>
2020-01-20 17:14:27 +0000

    Create sra-toolkit.sh

---
+++ sra-toolkit.sh
@@ -0,0 +1,5 @@
+wget "http://ftp-trace.ncbi.nlm.nih.gov/sra/sdk/current/sratoolkit.current-centos_linux64.tar.gz"
+
+tar -xzf sratoolkit.current-centos_linux64.tar.gz
+
+echo "You will want change the config by following this tutorial: https://ncbi.github.io/sra-tools/install_config.html. Because the default location will be: /home/[user_name]/ncbi/public"
